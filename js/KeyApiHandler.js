e1de847826175751c9ffae18f206d1932ba5e8aa
OctoPK <armand@biguey.com>
2025-10-01 09:51:11 +0200

    add URL and setAPI

--- js/KeyApiHandler.js
+++ js/KeyApiHandler.js
@@ -1,6 +1,22 @@
 let APIKey = null;
 let APIHost = "anime-db.p.rapidapi.com";
+const url = [
+    "https://anime-db.p.rapidapi.com/anime?",
+    "page=1",
+    "&",
+    "size=10",
+    "&",
+    "search=Fullmetal",
+    "&",
+    "genres=Fantasy%2CDrama",
+    "&",
+    "sortBy=ranking",
+    "&",
+    "sortOrder=asc",
+    "&",
+    "types=",
+];
 
-export function setAPI(key){
-    APIKey = key; 
+export function setAPI() {
+    APIKey = prompt("Entrez votre clé API pour commencez : ");
 }
