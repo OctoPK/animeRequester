764addf0f2367309eff7bf1bfd1c0cf11005e3cb
OctoPK <armand@biguey.com>
2025-10-08 10:31:57 +0200

    add setAPI in launch

--- app.js
+++ app.js
@@ -1,72 +1,73 @@
-const resultDisplay = document.getElementById('result');
-const effacerBtn = document.getElementById('effacer');
-const parametre = document.getElementById('parametre');
-const rechercheType = document.getElementById('recherche');
-const submitBtn = document.getElementById('submit');
-const titre = document.getElementById('titre');
-const sombre = document.getElementById('sombre');
+import { setAPI } from "./js/KeyApiHandler.js";
+
+const resultDisplay = document.getElementById("result");
+const effacerBtn = document.getElementById("effacer");
+const parametre = document.getElementById("parametre");
+const rechercheType = document.getElementById("recherche");
+const submitBtn = document.getElementById("submit");
+const titre = document.getElementById("titre");
+const sombre = document.getElementById("sombre");
 
 let cpt = 0;
 
-
-effacerBtn.addEventListener('click', () => {
-	parametre.value = "";
+effacerBtn.addEventListener("click", () => {
+    parametre.value = "";
 });
 
-submitBtn.addEventListener('click', () => {
-	if (recherche.value == 'nom') afficherNom();
-	if (recherche.value == 'identifiant') afficherID();
-	if (recherche.value == 'classement') afficherClassement();
+submitBtn.addEventListener("click", () => {
+    if (recherche.value == "nom") afficherNom();
+    if (recherche.value == "identifiant") afficherID();
+    if (recherche.value == "classement") afficherClassement();
 });
 
-sombre.addEventListener('click', () => {
-	if (cpt % 2 == 0){
-		document.body.style.backgroundColor = "DarkGrey";
-		document.body.style.color = "White";
-		cpt++;
-	} else {
-		document.body.style.backgroundColor = "White";
-		document.body.style.color = "Black";
-		cpt++;
-	}
+sombre.addEventListener("click", () => {
+    if (cpt % 2 == 0) {
+        document.body.style.backgroundColor = "DarkGrey";
+        document.body.style.color = "White";
+        cpt++;
+    } else {
+        document.body.style.backgroundColor = "White";
+        document.body.style.color = "Black";
+        cpt++;
+    }
 });
 
-function afficherNom(){
-	const nomAnime ="Anime";
-	if (nomAnime.includes(parametre.value)){
-		parametre.value = "";
-		titre.textContent = nomAnime;
-		afficherAnime();
-	} else{
-		titre.textContent ="Pas de resultat";
-	}
+function afficherNom() {
+    const nomAnime = "Anime";
+    if (nomAnime.includes(parametre.value)) {
+        parametre.value = "";
+        titre.textContent = nomAnime;
+        afficherAnime();
+    } else {
+        titre.textContent = "Pas de resultat";
+    }
 }
 
-function afficherID(){
-	const IDAnime ="0052";
-	if (IDAnime == parametre.value){
-		parametre.value = "";
-		titre.textContent = IDAnime;
-		afficherAnime();
-	} else{
-		titre.textContent ="Pas de resultat";
-	}
+function afficherID() {
+    const IDAnime = "0052";
+    if (IDAnime == parametre.value) {
+        parametre.value = "";
+        titre.textContent = IDAnime;
+        afficherAnime();
+    } else {
+        titre.textContent = "Pas de resultat";
+    }
 }
 
-function afficherClassement(){
-	const ClassementAnime ="103";
-	if (ClassementAnime == parametre.value){
-		parametre.value = "";
-		titre.textContent = ClassementAnime;
-		afficherAnime();
-	} else{
-		titre.textContent ="Pas de resultat";
-	}
+function afficherClassement() {
+    const ClassementAnime = "103";
+    if (ClassementAnime == parametre.value) {
+        parametre.value = "";
+        titre.textContent = ClassementAnime;
+        afficherAnime();
+    } else {
+        titre.textContent = "Pas de resultat";
+    }
 }
 
-function afficherAnime(){
+function afficherAnime() {}
 
-}
+setAPI();
 
 /*const url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc';
 const options = {
--- js/keymanager.js
+++
@@ -1,6 +0,0 @@
-import { setAPI } from './KeyApiHandler';
-
-export function getAPIkey(){
-    let key = "mettre la clé";
-    setAPI(key);
-}
