1b42e33742671fba399394fd980c20e594973fa6
BELMOND Tristan <belmondtristan@gmail.com>
2025-10-13 17:38:27 +0200

    Change of style to fix the website visual

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ rechercheType.addEventListener('change', () => {
 
 
 function afficheAnime (){
+
     section.innerHTML = "";
 
     let anime = [];
@@ -58,7 +59,6 @@ function afficheAnime (){
         let genre = document.createElement("p");
         let classement = document.createElement("p");
         let nb_episodes = document.createElement("p");
-        
         article.classList.add("anime");
 
         nom.textContent = anime[i]["title"];
@@ -74,9 +74,7 @@ function afficheAnime (){
         article.appendChild(genre);
         article.appendChild(classement);
         article.appendChild(nb_episodes);
-    
         section.appendChild(article);
-    
     
     }
 }
